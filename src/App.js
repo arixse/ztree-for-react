f3c91dd2ea90aeb15ec10f42f485147c501ed1c5
fusheng <fusheng@netposa.com>
2017-01-10 16:36:00 +0800

    lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import ReactZtree from '../lib/ztree-for-react';
 const nodes= [
-	{name: "父节点1",
+	{name: '父节点1',
     id:0,
      children: [
-		{name: "子节点1",
+		{name: '子节点1',
     id:1},
-		{name: "子节点2",id:2,children:[
-      {name:"子节点3",id:3}
+		{name: '子节点2',id:2,children:[
+      {name:'子节点3',id:3}
     ]}
 	]}
 ];
